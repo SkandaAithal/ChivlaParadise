9849632fd05d43fa77ca73145c88143badc040e6
SkandaAithal <skandaaithal1@gmail.com>
2025-08-18 14:19:55 +0530

    ui fixes

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,31 +1,27 @@
 "use client";
 import { motion } from "framer-motion";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import SwiperComponent from "./SwiperComponent";
 import { Autoplay, EffectCoverflow } from "swiper/modules";
 import { SwiperSlide } from "swiper/react";
 import Image from "next/image";
 import { CAROUSEL_IMAGES } from "@/lib/contants";
 import { FloatingPaths } from "./FloatingPaths";
+import { ChevronDown } from "lucide-react";
 
 const HeroBanner = () => {
-  const [isMounted, setIsMounted] = useState(false);
   const images = [...CAROUSEL_IMAGES, ...CAROUSEL_IMAGES, ...CAROUSEL_IMAGES];
   const initialSlideIndex = Math.floor(images.length / 2);
 
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
   return (
-    <section className="relative">
-      <div className="absolute inset-0 h-screen overflow-hidden translate-y-20 md:translate-y-0">
+    <section className="relative overflow-hidden">
+      <div className="absolute inset-0 min-h-screen overflow-hidden translate-y-20 md:translate-y-0">
         <FloatingPaths position={1} />
         <FloatingPaths position={-1} />
       </div>
-      <div className="min-h-screen bg-gradient-to-tr from-yellow-200 via-yellow-50 via-50% to-white grid grid-cols-1 place-content-center gap-10 px-4 md:px-8 lg:px-20">
+      <div className="min-h-screen bg-gradient-to-tr from-yellow-200 via-yellow-50 via-50% to-white grid grid-cols-1 place-content-center gap-10 md:gap-14 layout">
         <div className="flex justify-center flex-col items-center">
-          <h1 className="text-5xl lg:text-6xl drop-shadow-2xl font-semibold">
+          <h1 className="text-5xl lg:text-6xl drop-shadow-2xl font-semibold text-center">
             {"Chivla Paradise".split(" ").map((word, wordIndex) => (
               <span key={wordIndex} className="inline-block mr-4 last:mr-0">
                 {word.split("").map((letter, letterIndex) => (
@@ -39,7 +35,7 @@ const HeroBanner = () => {
                       stiffness: 150,
                       damping: 25,
                     }}
-                    className="inline-block text-green-900"
+                    className="inline-block text-[#009f70] "
                   >
                     {letter}
                   </motion.span>
@@ -48,13 +44,22 @@ const HeroBanner = () => {
             ))}
           </h1>
 
-          <p className="text-lg text-green-900">
+          <p className="text-green-900 text-xl md:text-3xl font-semibold drop-shadow-lg">
             Scenic Comfort by the Malvan Coast.
           </p>
         </div>
 
-        <div className="flex justify-center mask max-w-5xl mx-auto">
-          {isMounted ? (
+        <div className="flex justify-center w-full mask max-w-5xl mx-auto">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 1.2,
+              delay: 0.5,
+              ease: "easeOut",
+            }}
+            className="w-full"
+          >
             <SwiperComponent
               speed={600}
               centeredSlides={true}
@@ -114,9 +119,17 @@ const HeroBanner = () => {
                 );
               })}
             </SwiperComponent>
-          ) : (
-            <></>
-          )}
+          </motion.div>
+        </div>
+        <div
+          className="flex mx-auto justify-center items-center p-2 animate-bounce bg-white/20 w-fit rounded-full cursor-pointer"
+          onClick={() => {
+            document
+              .getElementById("spaces")
+              ?.scrollIntoView({ behavior: "smooth" });
+          }}
+        >
+          <ChevronDown className="h-6 w-6 text-yellow-800" />
         </div>
       </div>
     </section>
